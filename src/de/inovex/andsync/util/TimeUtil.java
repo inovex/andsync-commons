f4de45f1a936105162e6772a79f6ff2b87167715
Tim Roes <tim.roes@inovex.de>
2013-02-13 14:47:21 +0100

    Delete objects from cache, when deleted from server

---
+++ src/de/inovex/andsync/util/TimeUtil.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.inovex.andsync.util;
+
+/**
+ *
+ * @author Tim Roes <tim.roes@inovex.de>
+ */
+public class TimeUtil {
+	
+	public static long getTimestamp() {
+		return System.currentTimeMillis() / 1000;
+	}
+	
+}
