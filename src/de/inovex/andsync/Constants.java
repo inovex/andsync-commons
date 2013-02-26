59998642d4adc9aea6fdcbc1831946bfd2d8bea6
Tim Roes <tim.roes@inovex.de>
2013-02-26 09:06:57 +0100

    Only transfer new objects

--- src/de/inovex/andsync/Constants.java
+++ src/de/inovex/andsync/Constants.java
@@ -10,14 +10,29 @@ package de.inovex.andsync;
  */
 public class Constants {
 	
+	// Prevent instantiation or inheritance of this class
 	private Constants() { }
 	
 	public static final String UTF8 = "UTF-8";
 	
 	public static final String REST_OBJECT_PATH = "objects";
 	public static final String REST_FIELDS_KEY = "fields";
+	public static final String REST_MTIME_PATH = "mtime";
+	public static final String REST_META_PATH = "meta";
+	
+	public static final String REST_META_DELETION_PATH = "dtime";
+	public static final String REST_META_SIZE_PATH = "size";
 	
 	public static final String MONGO_ID = "_id";
+	public static final String MONGO_LAST_MODIFIED = "_mtime";
+	public static final String MONGO_META_CLASS = "class";
+	public static final String MONGO_META_DELETION = "dtime";
+	
+	/**
+	 * We won't use the HTTP standard Last-Modified, since that expects the date format to be
+	 * http://tools.ietf.org/html/rfc2822 but we just want to send timestamps.
+	 */
+	public static final String HTTP_MODIFIED_HEADER = "Last-Modification";
 	
 	public static final String LOG_TAG = "AndSync";
 	
